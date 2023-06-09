d1e36bd11df30f46e75ee4d09dfcf13b73aa69ee
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-06-10 01:33:18 +0530

    boon project created

---
+++ src/main/java/com/services/boon/BoonApplication.java
@@ -0,0 +1,13 @@
+package com.services.boon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoonApplication.class, args);
+    }
+
+}
