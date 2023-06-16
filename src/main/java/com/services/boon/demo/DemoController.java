9bd7a5e1bf15159ddc2bafc6ca6b96f20cc4acdd
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-06-17 01:43:59 +0530

    fixed the bug

--- src/main/java/com/services/boon/demo/DemoController.java
+++ src/main/java/com/services/boon/demo/DemoController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("api/v1/demo-controller")
+@RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
-    @GetMapping
-    public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello from secure endpoint");
+    @GetMapping()
+    public ResponseEntity<Map<String, Object>> sayHello(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "This is a sample response from the GET API");
+        response.put("status", "success");
+        return ResponseEntity.ok(response);
     }
 }
