fc95c1d18c3a5941bff2f027c33fa86b5edd01f1
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-07-30 22:46:08 +0530

    user and blog search implemented

---
+++ src/main/java/com/services/boon/search/UserSearchProjection.java
@@ -0,0 +1,7 @@
+package com.services.boon.search;
+
+public interface UserSearchProjection {
+     String getFirstname();
+     String getLastname();
+     String getUsername();
+}
--- src/main/java/com/services/boon/user/UserRepository.java
+++ src/main/java/com/services/boon/user/UserRepository.java
@@ -1,7 +1,11 @@
 package com.services.boon.user;
 
+import com.services.boon.search.UserSearchProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <User, Integer> {
@@ -9,4 +13,7 @@ public interface UserRepository extends JpaRepository <User, Integer> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    @Query(value = "select * from _user where lower(concat(firstname, lastname, username)) Like replace(lower(concat('%',:query,'%')), ' ', '')", nativeQuery = true)
+    Optional<List<UserSearchProjection>> searchUser(@Param("query") String query);
 }
